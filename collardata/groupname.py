176e39713823676d0880ac5e5fc602bc0dcb675f
cleo.collins.sl <cleo.collins.sl@a8677610-ea31-11dd-bd7b-b12b6590754f>
2010-05-19 18:39:45 +0000

    Adding lookup for groupnames

---
+++ collardata/groupname.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+#Licensed under the GPLv2 (not later versions)
+#see LICENSE.txt for details
+
+import cgi
+import os
+import re
+import lindenip
+import logging
+import string
+import wsgiref.handlers
+
+from google.appengine.api import urlfetch
+from google.appengine.ext import webapp
+
+import yaml
+
+
+
+class GetGroupName(webapp.RequestHandler):
+    def get(self):
+        if not lindenip.inrange(os.environ['REMOTE_ADDR']):
+            self.error(403)
+        else:
+            groupKey = self.request.get('group')
+            uplook_url = 'http://world.secondlife.com/group/%s' % groupKey
+            answer = urlfetch.fetch(uplook_url, method="GET", follow_redirects=True)
+            if answer.status_code == 200:
+                body = answer.content
+                if 'AccessDenied' in body:
+                    logging.info ('Group not found: %s' % groupKey)
+                    self.response.out.write('X')
+                else:
+                    startpos = string.find(body,"<title>") + 7
+                    endpos = string.find(body,"</title>")
+
+                    groupname = body[startpos:endpos]
+                    logging.info ('Group name for %s resolved: %s' % (groupKey, groupname))
+                    self.response.out.write(groupname)
+
+            else:
+                logging.info ('Error on group lookup answer: %d' % answer.status_code)
+                self.response.out.write('X')
+
+
+
+def main():
+  application = webapp.WSGIApplication([
+                                        (r'/.*?/GetGroupName',GetGroupName)
+                                        ], debug=True)
+  wsgiref.handlers.CGIHandler().run(application)
+
+
+if __name__ == '__main__':
+  main()
