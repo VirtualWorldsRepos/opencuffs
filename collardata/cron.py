22a3e0cde1947fa36dbdf416e08bd287a9118418
cleo.collins.sl <cleo.collins.sl@a8677610-ea31-11dd-bd7b-b12b6590754f>
2010-05-30 04:23:02 +0000

    Adding automatic vendor list for networked vendors

---
+++ collardata/cron.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+#Licensed under the GPLv2 (not later versions)
+#see LICENSE.txt for details
+
+import cgi
+import os
+import re
+import lindenip
+import logging
+import time
+
+
+import yaml
+
+import wsgiref.handlers
+from google.appengine.ext import db
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.api import memcache
+from google.appengine.api import urlfetch
+
+from model import VendorInfo
+import model
+
+# remove outdated vendors within 36-48 hours
+VendorRemoveTimeout = 129600
+
+class CleanVendors(webapp.RequestHandler):
+    def get(self):
+        t=int(time.time()) - VendorRemoveTimeout;
+        logging.info('CRON CleanVendors: Removing vendors older than %d' % t)
+        query = VendorInfo.gql("WHERE vendor_lastupdate < :1",  t)
+        for record in query:
+            logging.info('CRON: Vendor info for %s at %s outdated, removing it' % (record.vendor_key, record.vendor_slurl))
+            record.delete()
+        logging.info('CRON CleanVendors: Finished')
+
+
+
+def main():
+  application = webapp.WSGIApplication([
+                                        (r'/.*?/CleanVendors',CleanVendors)
+                                        ], debug=True)
+  wsgiref.handlers.CGIHandler().run(application)
+
+
+if __name__ == '__main__':
+  main()
+
---
+++ collardata/vendorlocs.py
@@ -0,0 +1,107 @@
+import cgi
+import urllib
+import logging
+import lindenip
+import os
+import relations
+import time
+import datetime
+import string
+from google.appengine.api import users
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext import db
+
+from model import VendorInfo
+
+head = '''
+<html>
+<head>
+<title>%s</title>
+<script src="/static/sorttable.js"></script>
+<style>
+body {
+    background-color: #000000;
+    color: #FF0000;
+    font-family:"Arial",Arial,sans-serif;
+}
+
+a:link { text-decoration:none; color:#e00000; }
+a:visited { text-decoration:none; color:#800000; }
+a:hover { text-decoration:none; background-color:#ff0; }
+
+input {
+    background-color: #000000;
+    color: #FF0000;
+    outline-color: #000000;
+    border-color: #FF0000;
+}
+table.sortable thead {
+    background-color:#202020;
+    color:#FF0000;
+    font-weight: bold;
+    cursor: default;
+}
+</style>
+</head>
+<body>
+<h1><img src='/static/OpenCollarLogo128.png' align="absmiddle">List of public available vendors of OpenCollar</h1>
+<p>In the following you find a list of all public accessible vendors. You can click on the headers of the table. to sort it for that column and use the SLURL to reach the veldors inworld.</p>
+<p>This list is automatically maintained, outdated vendors will be removed within 48 hours. The vendors there are usually not maintained by OpenCollar self.</p>
+'''
+
+end = '''
+</body>
+</html>
+'''
+
+
+class MainPage(webapp.RequestHandler):
+    def get(self):
+        query = VendorInfo.gql("WHERE vendor_public=1 ORDER BY vendor_agerating ASC, vendor_parcel ASC")
+        if query.count()==0:
+            message = '<b>Currently no vendors are listed!</b>'
+        else:
+            message = '<table class="sortable" border=\"1\">'
+            message += '<tr><th>Row</th><th>Parcel</th><th>AgeRating</th><th>SLURL</th></tr><br />\n'
+            content =[]
+            for record in query:
+                content += ['<td>%s</td><td>%s</td><td><a target="_blank" href="%s">%s</a></td>\n' % (record.vendor_parcel, record.vendor_agerating, record.vendor_slurl, record.vendor_slurl)]
+
+
+
+            #content = sorted(content)
+
+            for i in range(0,len(content)):
+                message += '<tr><td>%d</td>%s' % (i+1, content[i])
+
+            message += "</table>"
+
+        self.response.out.write((head % 'OpenCollar Vendor Locations') + message + end)
+
+
+application = webapp.WSGIApplication([
+    ('.*', MainPage)
+     ],
+    debug=True)
+
+def real_main():
+  run_wsgi_app(application)
+
+def profile_main():
+ # This is the main function for profiling
+ # We've renamed our original main() above to real_main()
+ import cProfile, pstats, StringIO
+ prof = cProfile.Profile()
+ prof = prof.runctx("real_main()", globals(), locals())
+ stream = StringIO.StringIO()
+ stats = pstats.Stats(prof, stream=stream)
+ stats.sort_stats("time")  # Or cumulative
+ stats.print_stats(80)  # 80 = how many to print
+ # The rest is optional.
+ # stats.print_callees()
+ # stats.print_callers()
+ logging.info("Profile data:\n%s", stream.getvalue())
+
+if __name__ == "__main__":
+  profile_main()
